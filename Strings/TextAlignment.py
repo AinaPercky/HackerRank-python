27f77e190832b5dc71cf5e0de6e3e4395205d97f
AinaPercky <ainapercky@gmail.com>
2025-01-09 15:41:50 +0300

    text alignment init

---
+++ Strings/TextAlignment.py
@@ -0,0 +1,22 @@
+n=int(input())
+n=n*n
+string='H'
+for i in range (0,10,2):
+    print((string.center(int(9),' ')))
+    string += 'HH'
+# print('H'.ljust(int(n),'-'))
+# print('H'.center(int(n),'-'))
+# print('H'.rjust(int(n),'-'))
+
+
+n=int(input())
+n=n*n
+string='H'
+for i in range (0,10,2):
+    print((string.center(int(9),' ')))
+    string += 'HH'
+# print('H'.ljust(int(n),'-'))
+# print('H'.center(int(n),'-'))
+# print('H'.rjust(int(n),'-'))
+
+
