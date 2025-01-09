9a620424ca0c316f4626859f021b1bdff5768fa3
AinaPercky <ainapercky@gmail.com>
2025-01-09 10:51:44 +0300

    list commands achieved

--- Lists/List.py
+++
@@ -1,9 +0,0 @@
-if __name__ == '__main__':
-    N = int(input())
-    list = []
-    for i in range(N):
-        command=input().split()
-        commanInstruction=command[0]
-        if(command[0]=='insert'):
-            list.insert(int(command[1]),int(command[2]))
-    print(list)
