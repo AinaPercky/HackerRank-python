ea61784777b4ad962b5db225ef08a5da4e91c703
AinaPercky <ainapercky@gmail.com>
2025-01-09 13:45:20 +0300

    update split join

--- Strings/SplitJoin.py
+++ Strings/SplitJoin.py
@@ -1,12 +1,5 @@
 def split_and_join(line):
-    lineTab=line.split()
-    joinedLine=''
-    for i in range(len(lineTab)):
-        if(i==len(lineTab)-1):
-            joinedLine=joinedLine+lineTab[i]
-        else:
-            joinedLine=joinedLine+lineTab[i]+'-'
-    return joinedLine
+    return '-'.join(line.split())
 
 if __name__ == '__main__':
     line = input()
