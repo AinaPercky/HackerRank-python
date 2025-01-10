00aab5dc04383409953dbaeee06b44930ab74b26
AinaPercky <ainapercky@gmail.com>
2025-01-10 11:24:33 +0300

    text alignment wip alignent down right

--- Lists/List.py
+++ Lists/List.py
@@ -2,21 +2,21 @@ if __name__ == '__main__':
     N = int(input())
     list = []
     for i in range(N):
-        command=input().split()
-        commanInstruction=command[0]
-        if(command[0]=='insert'):
-            list.insert(int(command[1]),int(command[2]))
-        elif(command[0]=='print'):
+        command = input().split()
+        commanInstruction = command[0]
+        if command[0] == 'insert':
+            list.insert(int(command[1]), int(command[2]))
+        elif command[0] == 'print':
             print(list)
-        elif(command[0]=='remove'):
+        elif command[0] == 'remove':
             list.remove(int(command[1]))
-        elif(command[0]=='append'):
+        elif command[0] == 'append':
             list.append(int(command[1]))
-        elif(command[0]=='sort'):
+        elif command[0] == 'sort':
             list.sort()
-        elif(command[0]=='pop'):
+        elif command[0] == 'pop':
             list.pop()
-        elif(command[0]=='reverse'):
+        elif command[0] == 'reverse':
             list.reverse()
         else:
             pass
--- Strings/TextAlignment.py
+++ Strings/TextAlignment.py
@@ -1,22 +1,40 @@
-n=int(input())
-n=n*n
-string='H'
-for i in range (0,10,2):
-    print((string.center(int(9),' ')))
-    string += 'HH'
-# print('H'.ljust(int(n),'-'))
-# print('H'.center(int(n),'-'))
-# print('H'.rjust(int(n),'-'))
+n = int(input())
+volume = n * n
+string = 'H'
+doubleString = string + string
+fiveString = string * 5
 
+def numberString(argstring, number):
+    i = 0
+    while i < (number - 1):
+        argstring += argstring[0]
+        i += 1
+    return argstring
 
-n=int(input())
-n=n*n
-string='H'
-for i in range (0,10,2):
-    print((string.center(int(9),' ')))
-    string += 'HH'
-# print('H'.ljust(int(n),'-'))
-# print('H'.center(int(n),'-'))
-# print('H'.rjust(int(n),'-'))
+def mutate_string(string, position, character):
+    string = list(string)
+    string[position] = character
+    string = ''.join(string)
+    return string
+
+for i in range(0, n * 2, 2):
+    print(string.center(9, ' '))
+    string += doubleString
+
+for i in range(n + 1):
+    print(fiveString.center(9, ' '), end="")
+    print('           ', end="")
+    print(fiveString.center(9, ' '))
+
+for i in range((n + 1) // 2):
+    print('  ', end="")
+    print(string.center(25, 'H'))
+
+for i in range(n + 1):
+    print(fiveString.center(9, ' '), end="")
+    print('           ', end="")
+    print(fiveString.center(9, ' '))
+
+print(numberString('H', 9).rjust(volume + 4, ' '))
 
 
