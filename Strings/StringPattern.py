3de2533d1de72184798666dec5f8a682178b4f8a
PERCKY <ainapercky@gmail.com>
2025-01-15 14:29:55 +0000

    remove unused space in triangle pattern

--- Strings/StringPattern.py
+++ Strings/StringPattern.py
@@ -9,16 +9,16 @@ def trianglePattern(level,char,desc=True,space=" ",spaceBefore=0):
         countSpace=0
         for i in range (count,0,-2):
             result=result+space*spaceBefore+space*countSpace+char*i+space*countSpace
-            result=result+'\n'
-            # print(space*countSpace+char*i+space*countSpace)
+            if (i>1):
+                result=result+'\n'
             countSpace+=1
     else:
         countSpace=(count//2)
         for i in range (1,count+1,2):
             result=result+space*spaceBefore+space*countSpace+char*i+space*countSpace
-            result=result+'\n'
-            print(space*countSpace+char*i+space*countSpace)
+            if(i<count):
+                result=result+'\n'
             countSpace-=1
     return result
 
-# print(trianglePattern(5,'*',True," ",10))
+print(trianglePattern(5,'*',False," ",10))
