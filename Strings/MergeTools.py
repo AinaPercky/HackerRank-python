63ac91dbdadca2245f95b5b149b3d227cf470e60
PERCKY <ainapercky@gmail.com>
2025-01-27 15:11:15 +0000

    merge tools achieved

--- Strings/MergeTools.py
+++ Strings/MergeTools.py
@@ -1,14 +1,13 @@
 def merge_the_tools(string, k):
-    split_number=len(string)//k
-    unique_chars=[]
+    splitted_k_string=[]
     for i in range(0,len(string),k):
         sub_string=string[i:i+k]
         unique_sub_string=[]
         for sub_string_item in sub_string:
             if( not(sub_string_item in unique_sub_string)):
                 unique_sub_string.append(sub_string_item)
-        unique_chars.append(unique_sub_string)
-    for letters in unique_chars:
+        splitted_k_string.append(unique_sub_string)
+    for letters in splitted_k_string:
         print(''.join(letters))
 
 
